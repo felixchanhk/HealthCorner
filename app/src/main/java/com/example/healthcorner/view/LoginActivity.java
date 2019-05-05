86bd87be409f42aeb711da2c877da6ed00b22a1a
felix chan <fcbusiness2017@gmail.com>
2019-05-05 16:23:16 +0800

    add login activity and xml file

--- app/src/main/java/com/example/healthcorner/view/LoginActivity.java
+++ app/src/main/java/com/example/healthcorner/view/LoginActivity.java
@@ -1,13 +1,15 @@
-package com.example.healthcorner;
+package com.example.healthcorner.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.healthcorner.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
